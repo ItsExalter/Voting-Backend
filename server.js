8d70543c1b358b848ddb16acac9cf7d77f5dad37
Mochammad Arrafie <Mochammad.Arrafie@Mattel.com>
2025-03-21 17:12:34 +0700

    Fix 'MongooseError: Can't call openUri() on an active connection with different connection strings. Make sure you aren't calling mongoose.connect() multiple times.'

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ const eventRoutes = require('./routes/event');
 const app = express();
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error(err));
+const dbConnection = mongoose.createConnection(process.env.MONGO_URI);
+
+dbConnection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+dbConnection.on('error', (err) => {
+  console.error(err);
+});
 
 // Middleware
 app.use(cors());
@@ -38,3 +44,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = { app };
